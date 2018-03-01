c9e005893e5e0d256129f72e6c1361c45ee2a81d
bermessa <bermessa@gmail.com>
2018-02-28 20:32:48 -0700

    fullpage app deploy test seperate files

---
+++ script.js
@@ -0,0 +1,9 @@
+
+        $(document).ready(function() {
+            $('#fullpage').fullpage({
+                // sectionsColor: ['#f2f2f2', '#4BBFC3', '#7BAABE', 'whitesmoke', '#000'],
+                navigation: true,
+                navigationPosition: 'right',
+                navigationTooltips: ['First page', 'Second page', 'Third and last page'],
+            });
+        });
