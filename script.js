08e98460464e14dcfb1afb74f71f9b9f949b8ac0
bermessa <bermessa@gmail.com>
2018-02-28 17:19:06 -0700

    fullpage

--- script.js
+++ script.js
@@ -1,16 +1,9 @@
 $(document).ready(function() {
 	$('#fullpage').fullpage({
         // sectionsColor: ['#f2f2f2', '#4BBFC3', '#7BAABE', 'whitesmoke', '#000'],
-    	// navigation: true,
-     //    navigationPosition: 'right',
-     //    navigationTooltips: ['First page', 'Second page', 'Third and last page']
-
-     navigation: true,
-     navigationPosition: 'right',
-     navigationTooltips: ['First page', 'Second page', 'Third and last page'],
-     responsiveWidth: 900,
-     afterResponsive: function(isResponsive){
-     	
-     }
- });
+    	navigation: true,
+        navigationPosition: 'right',
+        navigationTooltips: ['First page', 'Second page', 'Third and last page'],
+        fitToSection: true
+   	});
 });
