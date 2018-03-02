4dcc4cd90a3f845945d1205b171f945fff811afc
bermessa <bermessa@gmail.com>
2018-03-01 22:32:09 -0700

    section1 and headerader styles

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 $(document).ready(function() {
 	$('#fullpage').fullpage({
-		anchors:['page1', 'page2', 'page3'],
+		anchors:['page1', 'page2', 'page3', 'page4', 'page5'],
 		navigation: true,
 		navigationPosition: 'right',
 		navigationTooltips: ['First page', 'Second page', 'Third and last page']
